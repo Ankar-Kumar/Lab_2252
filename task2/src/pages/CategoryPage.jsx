0f2475bc08189f50ed4b2673a2e69d853541b001
Ankar Kumar Saha <ankarkumarankar572@gmial.com>
2026-02-02 16:25:37 +0600

    Update src/pages/CategoryPage.jsx page-component

--- task2/src/pages/CategoryPage.jsx
+++ task2/src/pages/CategoryPage.jsx
@@ -1,41 +1,55 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function CategoryPage() {
+    // usestate for storing the category data
+    const [categories, setCategories] = useState([]);
+    const fetchCategories = async () => {
+        const response = await fetch(
+            "https://www.themealdb.com/api/json/v1/1/categories.php"
+        );
+        const data = await response.json();
+        setCategories(data.categories);
+    };
 
     return (
-        <div className="container">
-            <div className="row pt-3">
-                <div className="col-12">
-                    <button className="btn btn-primary" >View Meal Categories</button>
-                </div>
-            </div>
-            <div>
-                <div>
-                    {/*Example category template-start*/}
-                    <div className="row my-4 shadow-sm bg-white py-3">
-                        <div className="col-3 margin-bottom-10">
-                            <img src="https://www.themealdb.com/images/category/beef.png" className={"w-100"}
-                                 alt="alt text"/>
-                        </div>
-                        <div className="col-9 p-2">
-                            <h2> Example Meal Category  Template </h2>
-                            <p>Dummy food Description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda cum quasi reiciendis
-                                rerum. Accusamus aliquid aspernatur assumenda cum deleniti distinctio dolore dolores,
-                                enim esse eveniet explicabo, fuga fugit illo ipsum iste itaque iure laudantium libero
-                                maiores mollitia natus neque nesciunt numquam obcaecati odio quae quam quas recusandae
-                                rem repudiandae rerum temporibus ut voluptatibus. Aliquid animi consectetur corporis
-                                cumque deleniti, dicta distinctio dolor dolores, </p>
+			<div className='container'>
+				<div className='row pt-3'>
+					<div className='col-12'>
+						<button className='btn btn-primary' onClick={fetchCategories}>
+							View Meal Categories
+						</button>
+					</div>
+				</div>
+				<div>
+					{/*Example category template-start*/}
+					{categories.map((category) => (
+						<div
+							key={category.idCategory}
+							className='row my-4 shadow-sm bg-white py-3'>
+							<div className='col-3 margin-bottom-10'>
+								<img
+									src={category.strCategoryThumb}
+									className={'w-100'}
+									alt={category.strCategory}
+								/>
+							</div>
+							<div className='col-9 p-2'>
+								<h2> {category.strCategory} </h2>
+								<p>{category.strCategoryDescription.substring(0, 250)}</p>
 
-                            <Link className={"bi-arrow-right-circle text-decoration-none"}
-                                  to={'/'}> View Foods</Link>
-                        </div>
-                    </div>
-                    {/*Example category template-end*/}
-                </div>
-            </div>
-        </div>
-    );
+								<Link
+									to={`/meals/${category.strCategory}`}
+									className='text-decoration-none'>
+									View Foods
+								</Link>
+							</div>
+						</div>
+						// {/*Example category template-end*/}
+					))}
+				</div>
+			</div>
+		);
 }
 
 export default CategoryPage;
